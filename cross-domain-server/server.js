8eaeebe86eae3d6b56b9a2b29c649562d0a50a3d
fistvan1984 <f.istvan1984@gmail.com>
2016-11-26 17:52:35 +0100

    init

---
+++ cross-domain-server/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var port = 3000;
+
+// Only google.com is allowed to send CORS request
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "google.com");
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port function() {
+	console.log('Server listening on port:', port);
+});
